317305701b19471ebbe9e7c3e92ac2e0e0c67cff
somyungsub <gkdldy5@naver.com>
2018-07-17 17:30:54 +0900

    Add : 3일차 교육예제 추가

---
+++ src/day03/Main.java
@@ -0,0 +1,11 @@
+package day03;
+
+import day03.exam.Test01;
+
+public class Main {
+    public static void main(String[] args) {
+        Test01 test01 = new Test01();
+        System.out.println(Test01.classVariable);   // public static 접근
+        System.out.println(test01.a);   // public 접근
+    }
+}
---
+++ src/day03/exam/Main.java
@@ -0,0 +1,10 @@
+package day03.exam;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Test01 test = new Test01();
+        System.out.println(test.a);
+        System.out.println(test.c);
+    }
+}
