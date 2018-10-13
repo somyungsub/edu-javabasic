c33ed64e239a7b4f100f77d9ef66f2b8367b7c69
somyungsub <gkdldy5@naver.com>
2018-10-14 00:56:16 +0900

    Add : homework/day03	 계좌관리플그램 추가

---
+++ src/homework/day03/bank/Account.java
@@ -0,0 +1,39 @@
+package homework.day03.bank;
+
+public class Account {
+    private String ano;		// 계좌번호
+    private String owner;	// 예금		
+    private int balance;	// 잔액
+
+    public Account(String ano, String owner, int balance) {
+        this.ano = ano;
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    
+    // getter, setter
+    public String getAno() {
+        return ano;
+    }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
